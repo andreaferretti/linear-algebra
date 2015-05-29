371cf5720465882790fc28ceba2fda140b8b04fd
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-05-29 16:56:48 +0200

    minor

--- buildc.sh
+++ buildc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# gcc -w -O3 -fno-strict-aliasing /home/papillon/esperimenti/blas/matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm
-gcc -w -O3 -fno-strict-aliasing /home/papillon/esperimenti/blas/matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lgomp -lm
+# gcc -w -O3 -fno-strict-aliasing matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm
+gcc -w -O3 -fno-strict-aliasing matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lgomp -lm
