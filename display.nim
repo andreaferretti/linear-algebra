423c1ddd05819237e87d3eca4f5e6c67fbf39355
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-06-03 15:32:32 +0200

    Better pretty print

--- display.nim
+++ display.nim
@@ -1,3 +1,7 @@
-proc `$`*(v: Vector64): string = repr(v.p[])
+proc `$`*(v: Vector64): string =
+  result = "[ "
+  for i in 0 .. < Vector64.N - 1:
+    result &= $(v[i]) & "\n  "
+  result &= $(v[Vector64.N - 1]) & " ]"
 
 proc `$`*(m: Matrix64): string = repr(m.p[])
