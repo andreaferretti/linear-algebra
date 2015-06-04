213a442ca686f4f49f34cfa51eb0eb909a936d82
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-06-04 17:39:22 +0200

    Minor

--- main.nim
+++ main.nim
@@ -6,7 +6,7 @@ proc main() =
     mat2 = randomMatrix(987, 876)
     mat3 = randomMatrix(4, 3)
     mat4 = randomMatrix(4, 3)
-    v1 = randomVector(10)
+    v = randomVector(10)
 
   let startTime = epochTime()
   for i in 0 .. < 10:
@@ -17,12 +17,12 @@ proc main() =
   for c in columns(mat3):
     echo c
 
-  echo "v1 = ", v1
-  echo "2 * v1 = ", 2 * v1
-  echo "id * v1 = ", eye(10) * v1
-  echo "l_1(v1) = ", l_1(v1)
-  echo "l_2(v1) = ", l_2(v1)
-  echo "l_2(v1)^2 = ", v1 * v1
+  echo "v = ", v
+  echo "2 * v = ", 2 * v
+  echo "id * v = ", eye(10) * v
+  echo "l_1(v) = ", l_1(v)
+  echo "l_2(v) = ", l_2(v)
+  echo "l_2(v)^2 = ", v * v
 
   echo mat3 * mat4.t
   echo mat3.t * mat4
