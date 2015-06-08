c346f06a45112e10c2be293f6056c8e5f806d41b
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-06-09 01:25:53 +0200

    Added the first tests

--- private/access.nim
+++ private/access.nim
@@ -1,3 +1,5 @@
+proc len*[N: static[int]](v: Vector64[N]): int = N
+
 proc at*[M, N: static[int]](m: Matrix64[M, N], i, j: int): float64 {. inline .} =
   if m.order == colMajor:
     let data = cast[ref array[N, array[M, float64]]](m.data)
@@ -27,3 +29,5 @@ proc row*[M, N: static[int]](m: Matrix64[M, N], i: int): Vector64[N] {. inline .
   new result
   for j in 0 .. < N:
     result[j] = m.at(i, j)
+
+proc dim*[M, N: static[int]](m: Matrix64[M, N]): tuple[rows, columns: int] = (M, N)
