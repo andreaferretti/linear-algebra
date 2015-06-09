1e88f87e0a3934f6e0e51a6d7d3245b36f3e403c
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-06-10 00:48:32 +0200

    Added copyright headers

--- linalg.nim
+++ linalg.nim
@@ -1,5 +1,3 @@
-from math import random
-
 # Copyright 2015 UniCredit S.p.A.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,7 @@ from math import random
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from math import random
 
 include private/external
 include private/blas
--- private/access.nim
+++ private/access.nim
@@ -1,3 +1,17 @@
+# Copyright 2015 UniCredit S.p.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 proc len*[N: static[int]](v: Vector64[N]): int = N
 
 proc at*[M, N: static[int]](m: Matrix64[M, N], i, j: int): float64 {. inline .} =
