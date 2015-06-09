a8de55331fdf74948f467cc9b04b4de6c8b460bb
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-06-09 18:24:56 +0200

    Removed extraneous stuff

--- other/buildc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# gcc -w -O3 -fno-strict-aliasing matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm
-gcc -w -O3 -fno-strict-aliasing matrices.c -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lgomp -lm
