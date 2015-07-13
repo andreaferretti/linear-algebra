86d44a970302780f2b89dc5ff80a7292425b968a
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-07-13 12:17:00 +0200

    Added some basic CUBLAS functions

--- cublas.nim
+++ cublas.nim
@@ -1,88 +1,102 @@
 when defined(cublas):
   {. passl: "-lcublas" passl: "-lcudart" .}
 
-  type cudaError = enum
-    cudaSuccess                           =      0
-    cudaErrorMissingConfiguration         =      1
-    cudaErrorMemoryAllocation             =      2
-    cudaErrorInitializationError          =      3
-    cudaErrorLaunchFailure                =      4
-    cudaErrorPriorLaunchFailure           =      5
-    cudaErrorLaunchTimeout                =      6
-    cudaErrorLaunchOutOfResources         =      7
-    cudaErrorInvalidDeviceFunction        =      8
-    cudaErrorInvalidConfiguration         =      9
-    cudaErrorInvalidDevice                =     10
-    cudaErrorInvalidValue                 =     11
-    cudaErrorInvalidPitchValue            =     12
-    cudaErrorInvalidSymbol                =     13
-    cudaErrorMapBufferObjectFailed        =     14
-    cudaErrorUnmapBufferObjectFailed      =     15
-    cudaErrorInvalidHostPointer           =     16
-    cudaErrorInvalidDevicePointer         =     17
-    cudaErrorInvalidTexture               =     18
-    cudaErrorInvalidTextureBinding        =     19
-    cudaErrorInvalidChannelDescriptor     =     20
-    cudaErrorInvalidMemcpyDirection       =     21
-    cudaErrorAddressOfConstant            =     22
-    cudaErrorTextureFetchFailed           =     23
-    cudaErrorTextureNotBound              =     24
-    cudaErrorSynchronizationError         =     25
-    cudaErrorInvalidFilterSetting         =     26
-    cudaErrorInvalidNormSetting           =     27
-    cudaErrorMixedDeviceExecution         =     28
-    cudaErrorCudartUnloading              =     29
-    cudaErrorUnknown                      =     30
-    cudaErrorNotYetImplemented            =     31
-    cudaErrorMemoryValueTooLarge          =     32
-    cudaErrorInvalidResourceHandle        =     33
-    cudaErrorNotReady                     =     34
-    cudaErrorInsufficientDriver           =     35
-    cudaErrorSetOnActiveProcess           =     36
-    cudaErrorInvalidSurface               =     37
-    cudaErrorNoDevice                     =     38
-    cudaErrorECCUncorrectable             =     39
-    cudaErrorSharedObjectSymbolNotFound   =     40
-    cudaErrorSharedObjectInitFailed       =     41
-    cudaErrorUnsupportedLimit             =     42
-    cudaErrorDuplicateVariableName        =     43
-    cudaErrorDuplicateTextureName         =     44
-    cudaErrorDuplicateSurfaceName         =     45
-    cudaErrorDevicesUnavailable           =     46
-    cudaErrorInvalidKernelImage           =     47
-    cudaErrorNoKernelImageForDevice       =     48
-    cudaErrorIncompatibleDriverContext    =     49
-    cudaErrorPeerAccessAlreadyEnabled     =     50
-    cudaErrorPeerAccessNotEnabled         =     51
-    cudaErrorDeviceAlreadyInUse           =     54
-    cudaErrorProfilerDisabled             =     55
-    cudaErrorProfilerNotInitialized       =     56
-    cudaErrorProfilerAlreadyStarted       =     57
-    cudaErrorProfilerAlreadyStopped       =     58
-    cudaErrorAssert                       =     59
-    cudaErrorTooManyPeers                 =     60
-    cudaErrorHostMemoryAlreadyRegistered  =     61
-    cudaErrorHostMemoryNotRegistered      =     62
-    cudaErrorOperatingSystem              =     63
-    cudaErrorPeerAccessUnsupported        =     64
-    cudaErrorLaunchMaxDepthExceeded       =     65
-    cudaErrorLaunchFileScopedTex          =     66
-    cudaErrorLaunchFileScopedSurf         =     67
-    cudaErrorSyncDepthExceeded            =     68
-    cudaErrorLaunchPendingCountExceeded   =     69
-    cudaErrorNotPermitted                 =     70
-    cudaErrorNotSupported                 =     71
-    cudaErrorHardwareStackError           =     72
-    cudaErrorIllegalInstruction           =     73
-    cudaErrorMisalignedAddress            =     74
-    cudaErrorInvalidAddressSpace          =     75
-    cudaErrorInvalidPc                    =     76
-    cudaErrorIllegalAddress               =     77
-    cudaErrorInvalidPtx                   =     78
-    cudaErrorInvalidGraphicsContext       =     79
-    cudaErrorStartupFailure               =   0x7f
-    cudaErrorApiFailureBase               =  10000
+  type
+    cudaError = enum
+      cudaSuccess                           =      0
+      cudaErrorMissingConfiguration         =      1
+      cudaErrorMemoryAllocation             =      2
+      cudaErrorInitializationError          =      3
+      cudaErrorLaunchFailure                =      4
+      cudaErrorPriorLaunchFailure           =      5
+      cudaErrorLaunchTimeout                =      6
+      cudaErrorLaunchOutOfResources         =      7
+      cudaErrorInvalidDeviceFunction        =      8
+      cudaErrorInvalidConfiguration         =      9
+      cudaErrorInvalidDevice                =     10
+      cudaErrorInvalidValue                 =     11
+      cudaErrorInvalidPitchValue            =     12
+      cudaErrorInvalidSymbol                =     13
+      cudaErrorMapBufferObjectFailed        =     14
+      cudaErrorUnmapBufferObjectFailed      =     15
+      cudaErrorInvalidHostPointer           =     16
+      cudaErrorInvalidDevicePointer         =     17
+      cudaErrorInvalidTexture               =     18
+      cudaErrorInvalidTextureBinding        =     19
+      cudaErrorInvalidChannelDescriptor     =     20
+      cudaErrorInvalidMemcpyDirection       =     21
+      cudaErrorAddressOfConstant            =     22
+      cudaErrorTextureFetchFailed           =     23
+      cudaErrorTextureNotBound              =     24
+      cudaErrorSynchronizationError         =     25
+      cudaErrorInvalidFilterSetting         =     26
+      cudaErrorInvalidNormSetting           =     27
+      cudaErrorMixedDeviceExecution         =     28
+      cudaErrorCudartUnloading              =     29
+      cudaErrorUnknown                      =     30
+      cudaErrorNotYetImplemented            =     31
+      cudaErrorMemoryValueTooLarge          =     32
+      cudaErrorInvalidResourceHandle        =     33
+      cudaErrorNotReady                     =     34
+      cudaErrorInsufficientDriver           =     35
+      cudaErrorSetOnActiveProcess           =     36
+      cudaErrorInvalidSurface               =     37
+      cudaErrorNoDevice                     =     38
+      cudaErrorECCUncorrectable             =     39
+      cudaErrorSharedObjectSymbolNotFound   =     40
+      cudaErrorSharedObjectInitFailed       =     41
+      cudaErrorUnsupportedLimit             =     42
+      cudaErrorDuplicateVariableName        =     43
+      cudaErrorDuplicateTextureName         =     44
+      cudaErrorDuplicateSurfaceName         =     45
+      cudaErrorDevicesUnavailable           =     46
+      cudaErrorInvalidKernelImage           =     47
+      cudaErrorNoKernelImageForDevice       =     48
+      cudaErrorIncompatibleDriverContext    =     49
+      cudaErrorPeerAccessAlreadyEnabled     =     50
+      cudaErrorPeerAccessNotEnabled         =     51
+      cudaErrorDeviceAlreadyInUse           =     54
+      cudaErrorProfilerDisabled             =     55
+      cudaErrorProfilerNotInitialized       =     56
+      cudaErrorProfilerAlreadyStarted       =     57
+      cudaErrorProfilerAlreadyStopped       =     58
+      cudaErrorAssert                       =     59
+      cudaErrorTooManyPeers                 =     60
+      cudaErrorHostMemoryAlreadyRegistered  =     61
+      cudaErrorHostMemoryNotRegistered      =     62
+      cudaErrorOperatingSystem              =     63
+      cudaErrorPeerAccessUnsupported        =     64
+      cudaErrorLaunchMaxDepthExceeded       =     65
+      cudaErrorLaunchFileScopedTex          =     66
+      cudaErrorLaunchFileScopedSurf         =     67
+      cudaErrorSyncDepthExceeded            =     68
+      cudaErrorLaunchPendingCountExceeded   =     69
+      cudaErrorNotPermitted                 =     70
+      cudaErrorNotSupported                 =     71
+      cudaErrorHardwareStackError           =     72
+      cudaErrorIllegalInstruction           =     73
+      cudaErrorMisalignedAddress            =     74
+      cudaErrorInvalidAddressSpace          =     75
+      cudaErrorInvalidPc                    =     76
+      cudaErrorIllegalAddress               =     77
+      cudaErrorInvalidPtx                   =     78
+      cudaErrorInvalidGraphicsContext       =     79
+      cudaErrorStartupFailure               =   0x7f
+      cudaErrorApiFailureBase               =  10000
 
+    cublasStatus = enum
+      cublasStatusSuccess                   =      0
+      cublasStatusNotInitialized            =      1
+      cublasStatusAllocFailed               =      3
+      cublasStatusInvalidValue              =      7
+      cublasStatusArchMismatch              =      8
+      cublasStatusMappingError              =     11
+      cublasStatusExecutionFailed           =     13
+      cublasStatusInternalError             =     14
+      cublasStatusNotSupported              =     15
+      cublasStatusLicenseError              =     16
+
+    cublasHandle = ptr object
 
   proc cudaMalloc*(size: int): ptr float32 =
     var error: cudaError
@@ -90,5 +104,19 @@ when defined(cublas):
     if error != cudaSuccess:
       quit($(error))
 
-  proc rawCudaMalloc(p: ptr ptr, size: int): cudaError
-    {. header: "cuda_runtime_api.h", importc: "cudaMalloc" .}
+  proc cublasCreate*(): cublasHandle =
+    var stat: cublasStatus
+    {.emit: """stat = cublasCreate_v2(& `result`); """.}
+    if stat != cublasStatusSuccess:
+      quit($(stat))
+
+  proc cublasSetVector*(n, elemSize: int, x: pointer, incx: int,
+    devicePtr: pointer, incy: int): cublasStatus
+    {. header: "cublas_api.h", importc: "cublasSetVector" .}
+
+
+  # proc rawCudaMalloc(p: ptr ptr, size: int): cudaError
+  #   {. header: "cuda_runtime_api.h", importc: "cudaMalloc" .}
+
+  # proc rawCublasCreate(h: object): cublasStatus
+  #   {. header: "cublas_api.h", importc: "cublasCreate_v2" .}
--- main.nim
+++ main.nim
@@ -1,39 +1,13 @@
 import times, linalg
 
 proc main() =
-  let
-    mat1 = randomMatrix(1000, 987)
-    mat2 = randomMatrix(987, 876)
-    mat3 = randomMatrix(4, 3)
-    mat4 = randomMatrix(4, 3)
-    mat5 = randomMatrix(3, 3)
-    v = randomVector(10)
-
-  let startTime = epochTime()
-  for i in 0 .. < 10:
-    discard mat1 * mat2
-  let endTime = epochTime()
-  echo "We have required ", endTime - startTime, " seconds to multiply matrices 10 times."
-
-  for c in columns(mat3):
-    echo c
-
-  echo "v = ", v
-  echo "2 * v = ", 2 * v
-  echo "v - v = ", v - v
-  echo "id * v = ", eye(10) * v
-  echo "l_1(v) = ", l_1(v)
-  echo "l_2(v) = ", l_2(v)
-  echo "l_2(v)^2 = ", v * v
-
-  echo mat3 * mat4.t
-  echo mat3.t * mat4
-  echo mat3 + mat4
-  echo mat5 + mat5.t
-  echo mat5 - mat5.t
+  let v = randomVector(10)
 
   # var p: ptr ptr float32
-  var p = cudaMalloc(1000 * sizeof(ptr float32))
+  var p = cudaMalloc(10 * sizeof(ptr float32))
+  var h = cublasCreate()
+  # let stat = cublasSetVector(10, sizeof(float64), addr(v[]), 1, p, 1)
+  # echo stat
 
 when isMainModule:
   main()
