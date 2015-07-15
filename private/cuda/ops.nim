4070885808b45264ad4e0d38a40654e3212640ae
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-07-15 19:31:08 +0200

    minor

--- private/cuda/ops.nim
+++ private/cuda/ops.nim
@@ -1,19 +1,17 @@
 let handle {.global.} = cublasCreate()
 
+template check(stat: cublasStatus): stmt =
+  if stat != cublasStatusSuccess:
+    quit($(stat))
+
 proc gpu*[N: static[int]](v: Vector32[N]): CudaVector[N] =
   new result
   result[] = cudaMalloc(N * sizeof(float32))
-  let stat = cublasSetVector(N, sizeof(float32), v.fp, 1, result[], 1)
-  if stat != cublasStatusSuccess:
-    quit($(stat))
+  check cublasSetVector(N, sizeof(float32), v.fp, 1, result[], 1)
 
 proc cpu*[N: static[int]](v: CudaVector[N]): Vector32[N] =
   new result
-  let stat = cublasGetVector(N, sizeof(float32), v[], 1, result.fp, 1)
-  if stat != cublasStatusSuccess:
-    quit($(stat))
+  check cublasGetVector(N, sizeof(float32), v[], 1, result.fp, 1)
 
 proc `+=`*[N: static[int]](v: var CudaVector[N], w: CudaVector[N]) {. inline .} =
-  let stat = cublasSaxpy(handle, N, 1, w[], v[])
-  if stat != cublasStatusSuccess:
-    quit($(stat))
+  check cublasSaxpy(handle, N, 1, w[], v[])
