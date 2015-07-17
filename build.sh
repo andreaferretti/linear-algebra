07f00a3c99e4ab6ba1c627c099b42408609e937b
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-07-17 12:09:04 +0200

    Removed files that are no more useful

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nim c -d:atlas -d:cublas --parallelBuild:1 --cincludes:"/usr/local/cuda-7.0/targets/x86_64-linux/include" --clibdir:"/usr/local/cuda-7.0/targets/x86_64-linux/lib" -d:release main.nim
-# nim c -d:mkl -d:release main.nim
-# nim c -d:mkl -d:threaded -d:release main.nim
-# nim c -d:atlas -d:release --profiler:on --stackTrace:on blas.nim
