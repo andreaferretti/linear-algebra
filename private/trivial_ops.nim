56438c791fad06f547a447ebd1bc0b1c6a61aeea
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-12-30 19:07:49 +0100

    Refactoring

--- private/trivial_ops.nim
+++ private/trivial_ops.nim
@@ -12,72 +12,70 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-template transposeStatic(a, result: expr) =
+template transposeS(a, result: expr) =
   result.order = if a.order == rowMajor: colMajor else: rowMajor
   result.data = a.data
 
-template transposeDynamic(a, result: expr) =
+template transposeD(a, result: expr) =
   result.M = a.N
   result.N = a.M
-  transposeStatic(a, result)
+  transposeS(a, result)
 
 proc t*[M, N: static[int]](a: Matrix32[M, N]): Matrix32[N, M] =
-  transposeStatic(a, result)
+  transposeS(a, result)
 
-proc t*(a: DMatrix32): DMatrix32 = transposeDynamic(a, result)
+proc t*(a: DMatrix32): DMatrix32 = transposeD(a, result)
 
 proc t*[M, N: static[int]](a: Matrix64[M, N]): Matrix64[N, M] =
-  transposeStatic(a, result)
+  transposeS(a, result)
 
-proc t*(a: DMatrix64): DMatrix64 = transposeDynamic(a, result)
+proc t*(a: DMatrix64): DMatrix64 = transposeD(a, result)
 
-template reshapeStatic(M, N, A, B, m , result: expr) =
+template reshapeS(M, N, A, B, m , result: expr) =
   static: doAssert(M * N == A * B, "The dimensions do not match: M = " & $(M) & ", N = " & $(N) & ", A = " & $(A) & ", B = " & $(B))
   result.order = m.order
   result.data = m.data
 
-proc reshape*[M, N: static[int]](m: Matrix32[M, N], A, B: static[int]): Matrix32[A, B] =
-  reshapeStatic(M, N, A, B, m, result)
-
-template reshapeDynamic(A, B, m , result: expr) =
+template reshapeD(A, B, m , result: expr) =
   assert(m.M * m.N == A * B, "The dimensions do not match: M = " & $(m.M) & ", N = " & $(m.N) & ", A = " & $(A) & ", B = " & $(B))
   result.M = A
   result.N = B
   result.order = m.order
   result.data = m.data
 
-proc reshape*(m: DMatrix32, A, B: int): DMatrix32 =
-  reshapeDynamic(A, B, m, result)
+proc reshape*[M, N: static[int]](m: Matrix32[M, N], A, B: static[int]): Matrix32[A, B] =
+  reshapeS(M, N, A, B, m, result)
+
+proc reshape*(m: DMatrix32, A, B: int): DMatrix32 = reshapeD(A, B, m, result)
 
 proc reshape*[M, N: static[int]](m: Matrix64[M, N], A, B: static[int]): Matrix64[A, B] =
-  reshapeStatic(M, N, A, B, m, result)
+  reshapeS(M, N, A, B, m, result)
 
-proc reshape*(m: DMatrix64, A, B: int): DMatrix64 =
-  reshapeDynamic(A, B, m, result)
+proc reshape*(m: DMatrix64, A, B: int): DMatrix64 = reshapeD(A, B, m, result)
+
+template asMatrixS(N, A, B, v, order, result: expr) =
+  static: doAssert(N == A * B, "The dimensions do not match: N = " & $(N) & ", A = " & $(A) & ", B = " & $(B))
+  result.order = order
+  result.data = v
+
+template asMatrixD(A, B, v, order, result: expr) =
+  assert(v.len == A * B, "The dimensions do not match: N = " & $(v.len) & ", A = " & $(A) & ", B = " & $(B))
+  result.order = order
+  result.data = v
+  result.M = A
+  result.N = B
 
 proc asMatrix*[N: static[int]](v: Vector32[N], A, B: static[int], order: OrderType = colMajor): Matrix32[A, B] =
-  static: doAssert(N == A * B, "The dimensions do not match: N = " & $(N) & ", A = " & $(A) & ", B = " & $(B))
-  result.order = order
-  result.data = v
+  asMatrixS(N, A, B, v, order, result)
 
 proc asMatrix*(v: DVector32, A, B: int, order: OrderType = colMajor): DMatrix32 =
-  assert(v.len == A * B, "The dimensions do not match: N = " & $(v.len) & ", A = " & $(A) & ", B = " & $(B))
-  result.order = order
-  result.data = v
-  result.M = A
-  result.N = B
+  asMatrixD(A, B, v, order, result)
 
 proc asMatrix*[N: static[int]](v: Vector64[N], A, B: static[int], order: OrderType = colMajor): Matrix64[A, B] =
-  static: doAssert(N == A * B, "The dimensions do not match: N = " & $(N) & ", A = " & $(A) & ", B = " & $(B))
-  result.order = order
-  result.data = v
+  asMatrixS(N, A, B, v, order, result)
 
 proc asMatrix*(v: DVector64, A, B: int, order: OrderType = colMajor): DMatrix64 =
-  assert(v.len == A * B, "The dimensions do not match: N = " & $(v.len) & ", A = " & $(A) & ", B = " & $(B))
-  result.order = order
-  result.data = v
-  result.M = A
-  result.N = B
+  asMatrixD(A, B, v, order, result)
 
 proc asVector*[M, N: static[int]](m: Matrix32[M, N]): Vector32[M * N] = m.data
 
