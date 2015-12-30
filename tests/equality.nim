976f90ebbe4cb7198b092f18bf3904015207374b
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-12-30 19:23:13 +0100

    More tests

--- tests/equality.nim
+++ tests/equality.nim
@@ -76,18 +76,18 @@ suite "vector and matrix equality":
     check n =~ p
     check n != p
     check p !=~ q
-  # test "approximate 32-bit matrix equality":
-  #   let
-  #     m = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #     n = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #     q = makeMatrix(3, 5, proc(i, j: int): float32 = (i - 2 * j).float32)
-  #   var p = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #   p[2, 2] = p[2, 2] - 0.000001
-  #   p[1, 3] = p[1, 3] + 0.000001
-  #   check m =~ n
-  #   check n =~ p
-  #   check n != p
-  #   check p !=~ q
+  test "approximate 32-bit matrix equality":
+    let
+      m = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
+      n = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
+      q = makeMatrix(3, 5, proc(i, j: int): float32 = (i - 2 * j).float32)
+    var p = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
+    p[2, 2] = p[2, 2] - 0.000001
+    p[1, 3] = p[1, 3] + 0.000001
+    check m =~ n
+    check n =~ p
+    check n != p
+    check p !=~ q
 
 
 suite "dynamic vector and matrix equality":
@@ -157,15 +157,17 @@ suite "dynamic vector and matrix equality":
     check n =~ p
     check n != p
     check p !=~ q
-  # test "approximate 32-bit matrix equality":
-  #   let
-  #     m = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #     n = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #     q = makeMatrix(3, 5, proc(i, j: int): float32 = (i - 2 * j).float32)
-  #   var p = makeMatrix(3, 5, proc(i, j: int): float32 = (i + 3 * j).float32)
-  #   p[2, 2] = p[2, 2] - 0.000001
-  #   p[1, 3] = p[1, 3] + 0.000001
-  #   check m =~ n
-  #   check n =~ p
-  #   check n != p
-  #   check p !=~ q
+  test "approximate 32-bit matrix equality":
+    let
+      M = 3
+      N = 5
+      m = makeMatrix(M, N, proc(i, j: int): float32 = (i + 3 * j).float32)
+      n = makeMatrix(M, N, proc(i, j: int): float32 = (i + 3 * j).float32)
+      q = makeMatrix(M, N, proc(i, j: int): float32 = (i - 2 * j).float32)
+    var p = makeMatrix(M, N, proc(i, j: int): float32 = (i + 3 * j).float32)
+    p[2, 2] = p[2, 2] - 0.000001
+    p[1, 3] = p[1, 3] + 0.000001
+    check m =~ n
+    check n =~ p
+    check n != p
+    check p !=~ q
