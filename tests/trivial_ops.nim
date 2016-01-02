914c84d2b19c83d2489e8b58e4aa308228f9358c
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-01-02 18:36:18 +0100

    Added tests

--- tests/trivial_ops.nim
+++ tests/trivial_ops.nim
@@ -158,3 +158,137 @@ suite "trivial dynamic operations":
         @[-1.0, 1.0, 4.0]
       ])
     check m1.t == m2
+
+suite "trivial operations should share storage":
+  test "reshape of matrices":
+    var
+      m1 = matrix([
+        [1.0, 0.0, 2.0, -1.0],
+        [-1.0, 1.0, 3.0, 1.0],
+        [3.0, 2.0, 2.0, 4.0]
+      ])
+      m2 = m1.reshape(4, 3)
+    m2[2, 1] = 0.0
+    check m1[0, 2] == 0.0
+  test "turn vectors into matrices":
+    var
+      v = vector([1.0, -1.0, 3.0, 0.0, 1.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 4.0])
+      m = v.asMatrix(3, 4)
+    m[2, 1] = 0.0
+    check v[5] == 0.0
+  test "turn matrices into vectors":
+    var
+      m = matrix([
+        [1.0, 0.0, 2.0, -1.0],
+        [-1.0, 1.0, 3.0, 1.0],
+        [3.0, 2.0, 2.0, 4.0]
+      ])
+      v = m.asVector
+    v[5] = 0.0
+    check m[2, 1] == 0.0
+  test "transpose of matrices":
+    var
+      m1 = matrix([
+        [1.0, 0.0, 2.0, -1.0],
+        [-1.0, 1.0, 3.0, 1.0],
+        [3.0, 2.0, 2.0, 4.0]
+      ])
+      m2 = m1.t
+    m2[1, 2] = 0.0
+    check m1[2, 1] == 0.0
+
+# suite "trivial operations on 32-bit matrices":
+#   test "reshape of matrices":
+#     let
+#       m1 = Matrix(3, 4, @[
+#         @[1'f32, 0'f32, 2'f32, -1'f32],
+#         @[-1'f32, 1'f32, 3'f32, 1'f32],
+#         @[3'f32, 2'f32, 2'f32, 4'f32]
+#       ])
+#       m2 = Matrix(4, 3, @[
+#         @[1'f32, 1'f32, 2'f32],
+#         @[-1'f32, 2'f32, -1'f32],
+#         @[3'f32, 2'f32, 1'f32],
+#         @[0'f32, 3'f32, 4'f32]
+#       ])
+#     check m1.reshape(4, 3) == m2
+#   test "turn vectors into matrices":
+#     let
+#       v = vector([1'f32, -1'f32, 3'f32, 0'f32, 1'f32, 2'f32, 2'f32, 3'f32, 2'f32, -1'f32, 1'f32, 4'f32], float32)
+#       m = Matrix(3, 4, @[
+#         @[1'f32, 0'f32, 2'f32, -1'f32],
+#         @[-1'f32, 1'f32, 3'f32, 1'f32],
+#         @[3'f32, 2'f32, 2'f32, 4'f32]
+#       ])
+#     check v.asMatrix(3, 4) == m
+#   test "turn matrices into vectors":
+#     let
+#       v = vector([1'f32, -1'f32, 3'f32, 0'f32, 1'f32, 2'f32, 2'f32, 3'f32, 2'f32, -1'f32, 1'f32, 4'f32], float32)
+#       m = Matrix(3, 4, @[
+#         @[1'f32, 0'f32, 2'f32, -1'f32],
+#         @[-1'f32, 1'f32, 3'f32, 1'f32],
+#         @[3'f32, 2'f32, 2'f32, 4'f32]
+#       ])
+#     check m.asVector == v
+#   test "transpose of matrices":
+#     let
+#       m1 = Matrix(3, 4, @[
+#         @[1'f32, 0'f32, 2'f32, -1'f32],
+#         @[-1'f32, 1'f32, 3'f32, 1'f32],
+#         @[3'f32, 2'f32, 2'f32, 4'f32]
+#       ])
+#       m2 = Matrix(4, 3, @[
+#         @[1'f32, -1'f32, 3'f32],
+#         @[0'f32, 1'f32, 2'f32],
+#         @[2'f32, 3'f32, 2'f32],
+#         @[-1'f32, 1'f32, 4'f32]
+#       ])
+#     check m1.t == m2
+#
+# suite "trivial dynamic operations":
+#   test "reshape of matrices":
+#     let
+#       m1 = matrix(@[
+#         @[1.0, 0.0, 2.0, -1.0],
+#         @[-1.0, 1.0, 3.0, 1.0],
+#         @[3.0, 2.0, 2.0, 4.0]
+#       ])
+#       m2 = matrix(@[
+#         @[1.0, 1.0, 2.0],
+#         @[-1.0, 2.0, -1.0],
+#         @[3.0, 2.0, 1.0],
+#         @[0.0, 3.0, 4.0]
+#       ])
+#     check m1.reshape(4, 3) == m2
+#   test "turn vectors into matrices":
+#     let
+#       v = @[1.0, -1.0, 3.0, 0.0, 1.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 4.0]
+#       m = matrix(@[
+#         @[1.0, 0.0, 2.0, -1.0],
+#         @[-1.0, 1.0, 3.0, 1.0],
+#         @[3.0, 2.0, 2.0, 4.0]
+#       ])
+#     check v.asMatrix(3, 4) == m
+#   test "turn matrices into vectors":
+#     let
+#       v = @[1.0, -1.0, 3.0, 0.0, 1.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 4.0]
+#       m = matrix(@[
+#         @[1.0, 0.0, 2.0, -1.0],
+#         @[-1.0, 1.0, 3.0, 1.0],
+#         @[3.0, 2.0, 2.0, 4.0]
+#       ])
+#     check m.asVector == v
+#   test "transpose of matrices":
+#     let
+#       m1 = matrix(@[
+#         @[1.0, 0.0, 2.0, -1.0],
+#         @[-1.0, 1.0, 3.0, 1.0],
+#         @[3.0, 2.0, 2.0, 4.0]
+#       ])
+#       m2 = matrix(@[
+#         @[1.0, -1.0, 3.0],
+#         @[0.0, 1.0, 2.0],
+#         @[2.0, 3.0, 2.0],
+#         @[-1.0, 1.0, 4.0]
+#       ])
+#     check m1.t == m2
