97aa8ddfcdf7a370c5bf8559fca82e25f5eea040
Sloane Simmons <sloane.simmons@rewardstyle.com>
2016-06-01 14:42:11 -0500

    Important random for v0.14.0 of compiler
    https://github.com/nim-lang/Nim/commit/95bb19a57938b75a58169f816856be0cad4474b9
    had a breaking change: `math.random` -> `random.random`.  Need to update
    imports so that will be able to compile.

--- linalg.nim
+++ linalg.nim
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math, nimblas
+import math, nimblas, random
 from sequtils import mapIt
 
 when defined(js):
