a4c0b2534ae7508122130f726c6f9bac579c18f7
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-11-12 19:31:22 +0100

    Updated copyright notice

--- bench/matrix_matrix_mult.nim
+++ bench/matrix_matrix_mult.nim
@@ -1,4 +1,4 @@
-# Copyright 2015 UniCredit S.p.A.
+# Copyright 2016 UniCredit S.p.A.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
