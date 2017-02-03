d3a13364cd071d7f81f647907afe54cb358f35df
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-02-03 18:14:08 +0100

    Added relu

--- cuda/linalg.cu
+++ cuda/linalg.cu
@@ -16,6 +16,42 @@ __global__ void hadamard_kernel_s(float* in1, float* in2, float* out, int count)
   }
 }
 
+__global__ void relu_kernel_d(double* in, double* out, int count)
+{
+  int index = threadIdx.x + blockIdx.x*blockDim.x;
+
+  if (index < count) {
+    out[index] = (in[index] < 0) ? 0 : in[index];
+  }
+}
+
+__global__ void relu_kernel_s(float* in, float* out, int count)
+{
+  int index = threadIdx.x + blockIdx.x*blockDim.x;
+
+  if (index < count) {
+    out[index] = (in[index] < 0) ? 0 : in[index];
+  }
+}
+
+__global__ void relu_prime_kernel_d(double* in, double* out, int count)
+{
+  int index = threadIdx.x + blockIdx.x*blockDim.x;
+
+  if (index < count) {
+    out[index] = (in[index] < 0) ? 0 : 1;
+  }
+}
+
+__global__ void relu_prime_kernel_s(float* in, float* out, int count)
+{
+  int index = threadIdx.x + blockIdx.x*blockDim.x;
+
+  if (index < count) {
+    out[index] = (in[index] < 0) ? 0 : 1;
+  }
+}
+
 extern "C" void hadamard_d(double* in1, double* in2, double* out, int count) {
     int dimGrid = 100;
     int dimBlock = 100;
@@ -28,4 +64,32 @@ extern "C" void hadamard_s(float* in1, float* in2, float* out, int count) {
     int dimBlock = 100;
 
     hadamard_kernel_s<<<dimGrid, dimBlock>>>(in1, in2, out, count);
+}
+
+extern "C" void relu_d(double* in, double* out, int count) {
+    int dimGrid = 100;
+    int dimBlock = 100;
+
+    relu_kernel_d<<<dimGrid, dimBlock>>>(in, out, count);
+}
+
+extern "C" void relu_s(float* in, float* out, int count) {
+    int dimGrid = 100;
+    int dimBlock = 100;
+
+    relu_kernel_s<<<dimGrid, dimBlock>>>(in, out, count);
+}
+
+extern "C" void relu_prime_d(double* in, double* out, int count) {
+    int dimGrid = 100;
+    int dimBlock = 100;
+
+    relu_prime_kernel_d<<<dimGrid, dimBlock>>>(in, out, count);
+}
+
+extern "C" void relu_prime_s(float* in, float* out, int count) {
+    int dimGrid = 100;
+    int dimBlock = 100;
+
+    relu_prime_kernel_s<<<dimGrid, dimBlock>>>(in, out, count);
 }
--- linalg/private/cuda/custom.nim
+++ linalg/private/cuda/custom.nim
@@ -4,6 +4,18 @@ proc hadamard(in1, in2, `out`: ptr float64, count: cint)
 proc hadamard(in1, in2, `out`: ptr float32, count: cint)
   {. cdecl, importc: "hadamard_s" , dynlib: "liblinalg.so" .}
 
+proc relu(`in`, `out`: ptr float32, count: cint)
+  {. cdecl, importc: "relu_s" , dynlib: "liblinalg.so" .}
+
+proc relu(`in`, `out`: ptr float64, count: cint)
+  {. cdecl, importc: "relu_d" , dynlib: "liblinalg.so" .}
+
+proc reluPrime(`in`, `out`: ptr float32, count: cint)
+  {. cdecl, importc: "relu_prime_s" , dynlib: "liblinalg.so" .}
+
+proc reluPrime(`in`, `out`: ptr float64, count: cint)
+  {. cdecl, importc: "relu_prime_d" , dynlib: "liblinalg.so" .}
+
 proc `|*|`*(v, w: CudaDVector32): CudaDVector32 {. inline .} =
   assert(v.N == w.N)
   initDynamic(result, v.N)
@@ -25,3 +37,39 @@ proc `|*|`*(v, w: CudaDMatrix64): CudaDMatrix64 {. inline .} =
   let (m, n) = v.dim
   initMDynamic(result, m, n)
   hadamard(v.fp, w.fp, result.fp, (m * n).cint)
+
+proc relu*(v: CudaDVector32): CudaDVector32 {. inline .} =
+  initDynamic(result, v.N)
+  relu(v.fp, result.fp, v.N.cint)
+
+proc relu*(v: CudaDVector64): CudaDVector64 {. inline .} =
+  initDynamic(result, v.N)
+  relu(v.fp, result.fp, v.N.cint)
+
+proc relu*(v: CudaDMatrix32): CudaDMatrix32 {. inline .} =
+  let (m, n) = v.dim
+  initMDynamic(result, m, n)
+  relu(v.fp, result.fp, (m * n).cint)
+
+proc relu*(v: CudaDMatrix64): CudaDMatrix64 {. inline .} =
+  let (m, n) = v.dim
+  initMDynamic(result, m, n)
+  relu(v.fp, result.fp, (m * n).cint)
+
+proc reluPrime*(v: CudaDVector32): CudaDVector32 {. inline .} =
+  initDynamic(result, v.N)
+  reluPrime(v.fp, result.fp, v.N.cint)
+
+proc reluPrime*(v: CudaDVector64): CudaDVector64 {. inline .} =
+  initDynamic(result, v.N)
+  reluPrime(v.fp, result.fp, v.N.cint)
+
+proc reluPrime*(v: CudaDMatrix32): CudaDMatrix32 {. inline .} =
+  let (m, n) = v.dim
+  initMDynamic(result, m, n)
+  reluPrime(v.fp, result.fp, (m * n).cint)
+
+proc reluPrime*(v: CudaDMatrix64): CudaDMatrix64 {. inline .} =
+  let (m, n) = v.dim
+  initMDynamic(result, m, n)
+  reluPrime(v.fp, result.fp, (m * n).cint)
